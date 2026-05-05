fb32918e67447fe69e5d43eb48c9cac351cad1a2
abirwalker <abirwalker@users.noreply.github.com>
2026-05-05 11:14:06 +0600

    package-lock

--- spice.config.ts
+++ spice.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from "@spicemod/creator"
 
 export default defineConfig({
   name: "Spicy Card",
-  identifier: "spicy-card",
-  version: "1.0.1",
-  description: "Beautiful Lyrics card view using hotwiring Spicy Lyrics API",
-  authors: ["abirwalker"],
+  template: "extension",
   entry: "./src/main.ts",
 })
